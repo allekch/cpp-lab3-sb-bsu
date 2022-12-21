d623e9f5a1239327f220b24e7d0e129b25a9d4dd
allekch <114610848+allekch@users.noreply.github.com>
2022-12-21 19:51:58 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -3,13 +3,21 @@ using namespace std;
 
 int main()
 {
-int k, i = 1, sum_k = 0, number;
+int k, i = 1, sum_k = 0;
+double number;
 cout << " Enter the amount of integers to sum = "; cin >> k;
 do {
 cout << "Enter integer nr. " << i << ": "; 
 cin >> number;
-sum_k += number; i++;
-} while (i <= k);
-cout << " The total sum of " << k << " integers is: "  << sum_k;
+while (number-(int)number!=0)
+{cout << "The number is not an integer " << endl;
+cout<<"Enter an another number =";
+cin>>number;
+break;
+}
+sum_k+= number; i++;
+}while (i<=k);
+cout <<"The total sum of"<<k<< "integers, i:" << sum_k;
 return 0;
 }
+
